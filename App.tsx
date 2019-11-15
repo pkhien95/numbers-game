3b55f8a35c2730e3cbc6f4ded5d0a99674ae8853
Hien Phung <hien@codelink.io>
2019-11-15 17:03:36 +0700

    add circle

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import Svg from "react-native-svg";
+import NumberCircle from "./src/NumberCircle";
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Svg width={'100%'} height={'100%'}>
+        <NumberCircle
+          cx={200}
+          cy={200}
+          number={1}
+          stroke="purple"
+          strokeWidth="2"
+          fill="red"
+        />
+      </Svg>
     </View>
   );
 }
